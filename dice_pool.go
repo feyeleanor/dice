bd01526d9362fbddc7dd1d77b5350fc6f17b5103
mrtnnbr <mrtnnbr@gmail.com>
2013-06-27 22:08:24 +0200

    Update dice_pool.go
    Fix rolls

--- dice_pool.go
+++ dice_pool.go
@@ -20,7 +20,7 @@ func NewDicePool(sides, number int) *DicePool {
 }
 
 func (d *DicePool) rand() int {
-	return rand.Intn(d.sides - 1) + 1
+	return rand.Intn(d.sides) + 1
 }
 
 func (d DicePool) Values() (r []int) {
@@ -48,7 +48,7 @@ func (d DicePool) Select(f Filter) (r int) {
 func (d *DicePool) Roll() {
 	if d.number > 0 {
 		if cap(d.values) == 0 {
-			d.values = make([]int, 0, d.number)
+			d.values = make([]int, d.number)
 		}
 		for n := d.number - 1; n > -1; n-- {
 			d.values[n] = d.rand()
